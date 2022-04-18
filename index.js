6fc9f1d271e0c447c17a51197f93e465520ea6b6
DRAYUB <98806823+DRAYUB@users.noreply.github.com>
2022-04-18 12:24:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save")
+let count = 0
+function increment () {
+    count = count + 1
+    countEl.innerText = count
+}
+function save () {
+    saveEl.textContent += " " + count +" - "
+    countEl.textContent = 0
+    count = 0
+}
+
+
